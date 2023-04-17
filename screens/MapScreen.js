c17c884985e648d3c3b2673eb66c7de3be326230
klarastromqvist <klara.stromqvist.6898@student.uu.se>
2023-04-17 11:53:02 +0200

    BottomTabs

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -133,10 +133,6 @@ export default function MapScreen({navigation}) {
           </Marker>
         ))}
       </MapView>
-
-      <View>
-      <Button title='Gå till Overview' onPress={navigateToScreen}/>
-      </View>
       
     </View>
     
@@ -150,7 +146,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: '100%',
-    height: '90%',
+    height: '100%',
   },
   nation: {
     color: 'black',
