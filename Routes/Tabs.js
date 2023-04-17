77a72e9363b62320262b338c8f58873b59a537c6
klarastromqvist <klara.stromqvist.6898@student.uu.se>
2023-04-17 14:40:28 +0200

    Styling ändringar för bottomTab

--- Routes/Tabs.js
+++ Routes/Tabs.js
@@ -1,18 +1,61 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../screens/HomeScreen';
 import MapScreen from '../screens/MapScreen';
 import OverViewScreen from '../screens/OverViewScreen';
 
-
-const Tab= createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const Tabs=() => {
 
     return(
-        <Tab.Navigator screenOptions={{ headerShown: false}}>
-            <Tab.Screen name="Home" component={HomeScreen} options={{tabBarStyle: {display: "none"},}}/>
-            <Tab.Screen name="Map" component={MapScreen}/>
-            <Tab.Screen name="OverView" component={OverViewScreen}/>
+        <Tab.Navigator 
+            screenOptions={{ 
+                headerShown: false,
+                tabBarStyle: { position: 'absolute'},
+                tabBarActiveTintColor: 'white',
+                tabBarInactiveTintColor: 'black',
+                tabBarActiveBackgroundColor: 'gray',
+                tabBarInactiveBackgroundColor: 'white',
+          }}
+            >
+            
+            <Tab.Screen 
+                name="Home" 
+                component={HomeScreen} 
+                options={{ 
+                    tabBarStyle: { display: 'none'},
+                    tabBarLabel: 'HOME',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="home" color={'black'} size={30} />
+                    ),  
+                }}
+                    
+                />
+
+            <Tab.Screen 
+                name="Map" 
+                component={MapScreen} 
+                options={{
+                    tabBarLabel: 'MAP',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="map-marker-radius" color={'black'} size={30} />
+                    ),
+                  }}
+               
+            />
+            
+            <Tab.Screen 
+                name="OverView" 
+                component={OverViewScreen}
+                options={{
+                    tabBarLabel: 'NATIONS',
+                    tabBarIcon: ({ color, size }) => (
+                      <MaterialCommunityIcons name="view-list" color={'black'} size={30} />
+                    ),
+                  }}
+            />
+        
         </Tab.Navigator>
     );
 }
